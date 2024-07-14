661dbdbd2a72c9f4267b56ebe3038afa7d819bb4
warzes <warzes@mail.ru>
2024-07-14 23:31:18 +1000

    - VkBootstrap

--- Project/FPS/main.cpp
+++ Project/FPS/main.cpp
@@ -1,11 +1,12 @@
 ﻿#if defined(_MSC_VER)
 #	pragma warning(push, 3)
-//#	pragma warning(disable : 5219)
+#	pragma warning(disable : 4191)
 #endif
 
 #define _USE_MATH_DEFINES
 
 #include <GLFW/glfw3.h>
+#include <VkBootstrap/VkBootstrap.h>
 
 #if defined(_MSC_VER)
 #	pragma warning(pop)
@@ -20,12 +21,23 @@ int main(
 	[[maybe_unused]] int   argc,
 	[[maybe_unused]] char* argv[])
 {
+	vkb::InstanceBuilder builder;
+	auto inst_ret = builder.set_app_name("Example Vulkan Application")
+		.request_validation_layers()
+		.use_default_debug_messenger()
+		.build();
+	if (!inst_ret)
+	{
+		//std::cerr << "Failed to create Vulkan instance. Error: " << inst_ret.error().message() << "\n";
+		return false;
+	}
+	vkb::Instance vkb_inst = inst_ret.value();
+
 	if (!glfwInit())
 	{
 		//Fatal("Failed to initialize GLFW");
 		return false;
 	}
 	//error: " + std::string(desc)); });
-
 }
 //-----------------------------------------------------------------------------
