d6f5417f56bd7042aeaa9a55c37f771e15ef47d6
warzes <warzes@mail.ru>
2024-07-21 19:48:12 +1000

    - фикс ошибки

--- Project/FPS/Renderer.cpp
+++ Project/FPS/Renderer.cpp
@@ -917,11 +917,12 @@ bool createDescriptorSetLayout()
 {
 	VkDescriptorSetLayoutBinding uboLayoutBinding{};
 	uboLayoutBinding.binding = 0;
-	uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 	uboLayoutBinding.descriptorCount = 1;
+	uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
 	uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
 
 	VkDescriptorSetLayoutCreateInfo layoutInfo{ .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO };
+	layoutInfo.bindingCount = 1;
 	layoutInfo.pBindings = &uboLayoutBinding;
 
 	if (vkCreateDescriptorSetLayout(Device, &layoutInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS)
@@ -1126,12 +1127,12 @@ void Renderer::Draw()
 		return;
 	}
 
+	updateUniformBuffer(CurrentFrame);
+
 	vkResetFences(Device, 1, &InFlightFences[CurrentFrame]);
 
 	vkResetCommandBuffer(CommandBuffers[CurrentFrame], /*VkCommandBufferResetFlagBits*/ 0);
 	recordCommandBuffer(CommandBuffers[CurrentFrame], imageIndex);
-
-	updateUniformBuffer(CurrentFrame);
 
 	VkSemaphore waitSemaphores[] = { ImageAvailableSemaphores[CurrentFrame] };
 	VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
