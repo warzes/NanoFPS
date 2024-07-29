ed7e47cfb9e9e76d0753a0e233472e55622e6501
warzes <warzes@mail.ru>
2024-07-29 15:31:21 +1000

    - обертка для GameHUD

---
+++ Project/FPS/GameHUD.cpp
@@ -0,0 +1,66 @@
+﻿#include "GameHUD.h"
+
+extern std::unique_ptr<PbrRenderer> renderer;
+
+GameHUD::GameHUD()
+{
+	m_textRenderer = new BitmapTextRenderer{ renderer.get(),
+		"textures/bitmap_fonts/share_tech_mono_shadow.png",
+		{ 23.0f, 48.0f }
+	};
+
+	m_whiteTexture = renderer->LoadSingleTextureMaterial("textures/dev/white.png");
+}
+
+GameHUD::~GameHUD()
+{
+	delete m_textRenderer;
+}
+
+void GameHUD::Update(float deltaTime)
+{
+	if (m_fadeTimer > 0.0f)
+		m_fadeTimer = glm::max(0.0f, m_fadeTimer - deltaTime);
+
+	if (m_objectiveNumDisplayedChars < m_objective.length())
+	{
+		if (m_objectivePrintTimer <= 0.0f)
+		{
+			m_objectivePrintTimer += 0.05f;
+			m_objectiveNumDisplayedChars++;
+			m_objectiveDisplayedText = m_objective.substr(0, m_objectiveNumDisplayedChars);
+			if (!std::isspace(m_objectiveDisplayedText.back()))
+			{
+				//Audio->PlayOneShot("event:/ui/objective/type");
+			}
+		}
+		m_objectivePrintTimer -= deltaTime;
+	}
+}
+
+void GameHUD::Draw()
+{
+	const glm::vec2 screenExtent = renderer->GetScreenExtent();
+
+	if (m_fadeTimer > 0.0f)
+	{
+		const float fade = m_fadeTimer / m_fadeDuration;
+		renderer->DrawScreenRect(
+			{ 0.0f, 0.0f },
+			screenExtent,
+			{ 0.0f, 0.0f },
+			{ 1.0f, 1.0f },
+			{ m_fadeColor.x,
+			m_fadeColor.y,
+			m_fadeColor.z,
+			m_fadeTo ? 1.0f - fade : fade },
+			m_whiteTexture
+		);
+	}
+
+	m_textRenderer->DrawText(
+		m_objectiveDisplayedText,
+		{ 48.0f, screenExtent.y - 48.0f - m_textRenderer->GetCharSize().y },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+	);
+}
---
+++ Project/FPS/GameHUD.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+#include "NanoEngineVK.h"
+
+class GameHUD final
+{
+public:
+	GameHUD();
+	GameHUD(GameHUD&&) = delete;
+	~GameHUD();
+	GameHUD(const GameHUD&) = delete;
+
+	GameHUD& operator=(const GameHUD&) = delete;
+	GameHUD& operator=(GameHUD&&) = delete;
+
+	void Update(float deltaTime);
+
+	void Draw();
+
+	void FadeToColor(float duration, const glm::vec3& color)
+	{
+		m_fadeDuration = duration;
+		m_fadeTimer = duration;
+		m_fadeTo = true;
+		m_fadeColor = color;
+	}
+
+	void FadeFromColor(float duration, const glm::vec3& color)
+	{
+		m_fadeDuration = duration;
+		m_fadeTimer = duration;
+		m_fadeTo = false;
+		m_fadeColor = color;
+	}
+
+	void ShowObjective(std::string objective)
+	{
+		m_objective = std::move(objective);
+		m_objectiveNumDisplayedChars = 0;
+		m_objectivePrintTimer = 0.0f;
+	}
+
+private:
+	BitmapTextRenderer* m_textRenderer;
+	SingleTextureMaterial* m_whiteTexture;
+
+	float       m_fadeDuration = 0.0f;
+	float       m_fadeTimer = 0.0f;
+	bool        m_fadeTo = false; // true: transparent->color; false: color->transparent
+	glm::vec3   m_fadeColor{};
+
+	std::string m_objective;
+	int         m_objectiveNumDisplayedChars = 0;
+	float       m_objectivePrintTimer = 0.0f;
+	std::string m_objectiveDisplayedText;
+};
