7aeaf3e642c6797e91728f7154cf470b99a1f59d
warzes <warzes@mail.ru>
2024-09-27 20:32:03 +1000

    - выделение примеров в отдельный проект

---
+++ Project/Example/main.cpp
@@ -0,0 +1,69 @@
+﻿#include "stdafx.h"
+#include "001Triangle.h"
+#include "002TriangleSpinning.h"
+#include "003_SquateTextured.h"
+#include "004_Cube.h"
+#include "005_CubeTextured.h"
+#include "006_ComputeFill.h"
+#include "007_DrawIndexed.h"
+#include "008_BasicGeometry.h"
+#include "009_ObjGeometry.h"
+#include "010_CubeMap.h"
+#include "011_CompressedTexture.h"
+#include "012_Shadows.h"
+#include "013_NormalMap.h"
+#include "014_ArcballCamera.h"
+#include "015_CameraMotion.h"
+#include "016_ImguiAndInput.h"
+#include "017_AsyncCompute.h"
+#include "018_DynamicRendering.h"
+#include "019_gltf.h"
+#include "020_GBuffer.h"
+#include "021_PushConstants.h"
+#include "022_PushDescriptors.h"
+#include "023_PushDescriptorsBuffers.h"
+#include "024_OIT.h"
+#include "025_BasicMaterial.h"
+#include "026_Text.h"
+#include "027_Mipmap.h"
+
+#if defined(_MSC_VER)
+#	pragma comment( lib, "NanoEngine.lib" )
+#	pragma comment( lib, "NanoEngine3rdparty.lib" )
+#endif
+//-----------------------------------------------------------------------------
+int main(
+	[[maybe_unused]] int   argc,
+	[[maybe_unused]] char* argv[])
+{
+	//Example_001 app;
+	//Example_002 app;
+	//Example_003 app;
+	//Example_004 app;
+	//Example_005 app;
+	//Example_006 app;
+	//Example_007 app;
+	//Example_008 app;
+	//Example_009 app;
+	//Example_010 app;
+	//Example_011 app;
+	//Example_012 app;
+	//Example_013 app;
+	//Example_014 app;
+	//Example_015 app;
+	//Example_016 app;
+	//Example_017 app;
+	//Example_018 app;
+	//Example_019 app;
+	//Example_020 app;
+	//Example_021 app;
+	//Example_022 app;
+	//Example_023 app;
+	//Example_024 app;
+	//Example_025 app;
+	//Example_026 app;
+	Example_027 app;
+
+	app.Run();
+}
+//-----------------------------------------------------------------------------
